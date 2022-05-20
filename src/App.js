e6732c2686c10fa774544549b7a42b6877bb270a
shimabukuro_k <shimabukuro_k@beanslabo.co.jp>
2022-05-20 15:30:35 +0900

    保存

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import React, {useState} from 'react';
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const onClickCount = () => {
+  const onClickCountUp = () => {
     setCount(count + 1 )
   };
+
+  const onClickCountDown = () => {
+    setCount(count - 1 )
+  };
+
+  const onClickCount2 = () => {
+    setCount(previousCount => previousCount +1)
+  };
+
+  const onClickReset = () => {
+    setCount(0);
+  };
+
+  const onClickMultiplication = () => {
+    setCount(count * 2);
+  };
+
+  const onClick3waru = () => setCount(previousCount => previousCount % 3 === 0 ? setCount(previousCount / 3) : previousCount)
 
   return (
     <>
     <div>count: {count}</div>
       <br/>
-      <button onClick={onClickCount}>カウントアップ</button>
+      <div>
+        <button onClick={onClickCountUp}>カウントアップ</button>
+        <button onClick={onClickCountDown}>カウントダウン</button>
+      </div>
+      <div>
+        <button onClick={onClickCount2}>カウントアップ2</button>
+      </div>
+      <div>
+        <button onClick={onClickReset}>Reset</button>
+        <button onClick={onClickMultiplication}>×2</button>
+        <button onClick={onClick3waru}>3の倍数の時だけ3でわる</button>
+      </div>
     </>
   );
 }
