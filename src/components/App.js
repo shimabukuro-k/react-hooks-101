1efae421f06ba3f7b5b0d2983e4ee0f2a878f8ff
shimabukuro_k <shimabukuro_k@beanslabo.co.jp>
2022-06-01 17:38:25 +0900

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, { useReducer, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reducer from "../reducers"
@@ -7,12 +7,20 @@ import EventList from "./EventList"
 import AppContext from '../contexts/AppContext';
 import OperationLog from './OperationLog';
 
+const APP_KIY = "appWithRedux";
+
 export const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KIY);
+
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem(APP_KIY , JSON.stringify(state));
+  }, [state])
 
   return (
     <AppContext.Provider value={{state, dispatch}}>
