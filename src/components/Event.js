3dc9ac8b37a3e388a219a851ba0ac2d6d0f2b55b
shimabukuro_k <shimabukuro_k@beanslabo.co.jp>
2022-06-01 16:29:40 +0900

    save

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,26 @@
 import React, { useContext } from "react"
 
-import { DELETE_EVENT } from "../actions"
+import {
+    DELETE_EVENT,
+    ADD_OPERATION_LOG
+} from "../actions"
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utilus";
+
 
 const Event = ({ event }) => {
     const { dispatch } = useContext(AppContext)
     const id = event.id;
     const deleteEvent = () => {
         const result = window.confirm(`ID${id}のイベントを削除しますか？`);
-        if (result) dispatch({ type:DELETE_EVENT, id })
+        if (result) {
+            dispatch({ type:DELETE_EVENT, id })
+            dispatch({
+                type:ADD_OPERATION_LOG,
+                description:`イベント(ID${id})を削除しました。`,
+                operatedAt:timeCurrentIso8601()
+            })
+        }
     }
 
     return (
